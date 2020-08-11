070aa13c50bd9e0f106c6e32038ae2059010aaff
Mahfujur Rahman <mrtbd88@gmail.com>
2020-08-11 17:58:04 -0400

    configuresd config class

--- src/base/Config.java
+++ src/base/Config.java
@@ -10,15 +10,14 @@ public class Config extends WDFunctions {
 
 @BeforeMethod
 public void beforeEachTestMethod(){
-	// Init the browser
-	System.setProperty("webdriver.chrome.driver", (System.getProperty("user.dir")+"/drivers/chromedriver.exe"));
-
+	
+//	System.setProperty("webdriver.chrome.driver", (System.getProperty("user.dir")+"/drivers/chromedriver.exe"));
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mahfuj Tuhin\\eclipse-workspace\\NN07\\drivers\\chromedriver.exe");
 	driver = new ChromeDriver();
 	driver.manage().deleteAllCookies();
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
-	System.out.println (" chrome driver open ++++++++++++++++ ");
-	// open browser	
+	System.out.println (" chrome driver open------------------");
 	driver.get("http://www.booking.com");
    }
 }
